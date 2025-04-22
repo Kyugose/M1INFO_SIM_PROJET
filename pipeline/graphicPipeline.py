9bbbd7568528e3e62b1055eed4e8b059ef57f370
Eric Chen <eric.chen@etu.univ-grenoble-alpes.fr>
2025-04-22 15:49:13 +0200

    msaa x4 bon lissage ,x8 à corriger

--- pipeline/graphicPipeline.py
+++ pipeline/graphicPipeline.py
@@ -27,47 +27,111 @@ class GraphicPipeline:
         self.msaaDepth = np.ones((height, width, self.samples))
         self.coverage = np.zeros((height, width), dtype=int)
 
-    def VertexShader(self, vertex, data):
+    def VertexShader(self, vertex, data) :
         outputVertex = np.zeros((14))
+
         x = vertex[0]
         y = vertex[1]
         z = vertex[2]
         w = 1.0
-        vec = np.array([[x], [y], [z], [w]])
-        vec = np.matmul(data['projMatrix'], np.matmul(data['viewMatrix'], vec))
-        outputVertex[0] = vec[0] / vec[3]
-        outputVertex[1] = vec[1] / vec[3]
-        outputVertex[2] = vec[2] / vec[3]
-        outputVertex[3:6] = vertex[3:6]
-        outputVertex[6:9] = data['cameraPosition'] - vertex[0:3]
-        outputVertex[9:12] = data['lightPosition'] - vertex[0:3]
-        outputVertex[12:14] = vertex[6:8]
+
+        vec = np.array([[x],[y],[z],[w]])
+
+        vec = np.matmul(data['projMatrix'],np.matmul(data['viewMatrix'],vec))
+
+        outputVertex[0] = vec[0]/vec[3]
+        outputVertex[1] = vec[1]/vec[3]
+        outputVertex[2] = vec[2]/vec[3]
+
+        outputVertex[3] = vertex[3]
+        outputVertex[4] = vertex[4]
+        outputVertex[5] = vertex[5]
+
+        outputVertex[6] = data['cameraPosition'][0] - vertex[0]
+        outputVertex[7] = data['cameraPosition'][1] - vertex[1]
+        outputVertex[8] = data['cameraPosition'][2] - vertex[2]
+
+        outputVertex[9] = data['lightPosition'][0] - vertex[0]
+        outputVertex[10] = data['lightPosition'][1] - vertex[1]
+        outputVertex[11] = data['lightPosition'][2] - vertex[2]
+
+        outputVertex[12] = vertex[6]
+        outputVertex[13] = vertex[7]
+
         return outputVertex
 
-    def Rasterizer(self, v0, v1, v2):
+
+    def Rasterizer(self, v0, v1, v2) :
         fragments = []
-        area = edgeSide(v0, v1, v2)
-        if area < 0:
+
+        #culling back face
+        area = edgeSide(v0,v1,v2)
+        if area < 0 :
             return fragments
-        verts = [v0, v1, v2]
-        verts_image = [((v[0] + 1.0) / 2.0 * self.width, (v[1] + 1.0) / 2.0 * self.height) for v in verts]
-        A = np.min(verts_image, axis=0).astype(int)
-        B = np.max(verts_image, axis=0).astype(int) + 1
-        for j in range(A[1], B[1]):
-            for i in range(A[0], B[0]):
-                x = (i + 0.5) / self.width * 2.0 - 1
-                y = (j + 0.5) / self.height * 2.0 - 1
-                p = np.array([x, y])
-                area0 = edgeSide(p, v0, v1)
-                area1 = edgeSide(p, v1, v2)
-                area2 = edgeSide(p, v2, v0)
-                if area0 >= 0 and area1 >= 0 and area2 >= 0:
-                    lambda0 = area1 / area
-                    lambda1 = area2 / area
-                    lambda2 = area0 / area
+        
+        
+        #AABBox computation
+        #compute vertex coordinates in screen space
+        v0_image = np.array([0,0])
+        v0_image[0] = (v0[0]+1.0)/2.0 * self.width 
+        v0_image[1] = ((v0[1]+1.0)/2.0) * self.height 
+
+        v1_image = np.array([0,0])
+        v1_image[0] = (v1[0]+1.0)/2.0 * self.width 
+        v1_image[1] = ((v1[1]+1.0)/2.0) * self.height 
+
+        v2_image = np.array([0,0])
+        v2_image[0] = (v2[0]+1.0)/2.0 * self.width 
+        v2_image[1] = (v2[1]+1.0)/2.0 * self.height 
+
+        #compute the two point forming the AABBox
+        A = np.min(np.array([v0_image,v1_image,v2_image]), axis = 0)
+        B = np.max(np.array([v0_image,v1_image,v2_image]), axis = 0)
+
+        #cliping the bounding box with the borders of the image
+        max_image = np.array([self.width-1,self.height-1])
+        min_image = np.array([0.0,0.0])
+
+        A  = np.max(np.array([A,min_image]),axis = 0)
+        B  = np.min(np.array([B,max_image]),axis = 0)
+        
+        #cast bounding box to int
+        A = A.astype(int)
+        B = B.astype(int)
+        #Compensate rounding of int cast
+        B = B + 1
+
+        #for each pixel in the bounding box
+        for j in range(A[1], B[1]) : 
+           for i in range(A[0], B[0]) :
+                x = (i+0.5)/self.width * 2.0 - 1 
+                y = (j+0.5)/self.height * 2.0 - 1
+
+                p = np.array([x,y])
+                
+                area0 = edgeSide(p,v0,v1)
+                area1 = edgeSide(p,v1,v2)
+                area2 = edgeSide(p,v2,v0)
+
+                #test if p is inside the triangle
+                if (area0 >= 0 and area1 >= 0 and area2 >= 0) : 
+                    
+                    #Computing 2d barricentric coordinates
+                    lambda0 = area1/area
+                    lambda1 = area2/area
+                    lambda2 = area0/area
+                    
+                    #one_over_z = lambda0 * 1/v0[2] + lambda1 * 1/v1[2] + lambda2 * 1/v2[2]
+                    #z = 1/one_over_z
                     z = lambda0 * v0[2] + lambda1 * v1[2] + lambda2 * v2[2]
-                    interpolated_data = v0[3:] * lambda0 + v1[3:] * lambda1 + v2[3:] * lambda2
-                    fragments.append(Fragment(i, j, z, interpolated_data))
+                    p = np.array([x,y,z])
+                    l = v0.shape[0]
+                    #interpolating
+                    interpolated_data = v0[3:l] * lambda0 + v1[3:l] * lambda1 + v2[3:l] * lambda2
+                    
+                    #Emiting Fragment
+                    fragments.append(Fragment(i,j,z, interpolated_data))
+
         return fragments
 
     def RasterizerMSAA(self, v0, v1, v2):
@@ -95,64 +159,119 @@ class GraphicPipeline:
                         fragments.append(Fragment(x, y, z, interp_data, s))
         return fragments
 
-    def fragmentShader(self, fragment, data):
+    def RasterizerMSAAx8(self, v0, v1, v2):
+        fragments = []
+        area = edgeSide(v0, v1, v2)
+        if area < 0:
+            return fragments
+        sample_positions = [
+            (0.0625, 0.1875), (0.3125, 0.4375), 
+            (0.6875, 0.0625), (0.9375, 0.3125),
+            (0.1875, 0.8125), (0.4375, 0.9375),
+            (0.8125, 0.5625), (0.5625, 0.6875)
+        ]
+    
+        verts_image = [((v[0] + 1) / 2 * self.width, (v[1] + 1) / 2 * self.height) for v in [v0, v1, v2]]
+        A = np.min(verts_image, axis=0).astype(int)
+        B = np.max(verts_image, axis=0).astype(int) + 1
+        for y in range(A[1], B[1]):
+            for x in range(A[0], B[0]):
+                for s, (dx, dy) in enumerate(sample_positions):
+                    px = ((x + dx) / self.width) * 2 - 1
+                    py = ((y + dy) / self.height) * 2 - 1
+                    p = np.array([px, py])
+                    e0 = edgeSide(p, v0, v1)
+                    e1 = edgeSide(p, v1, v2)
+                    e2 = edgeSide(p, v2, v0)
+                    if e0 >= 0 and e1 >= 0 and e2 >= 0:
+                        l0, l1, l2 = e1 / area, e2 / area, e0 / area
+                        z = l0 * v0[2] + l1 * v1[2] + l2 * v2[2]
+                        interp_data = l0 * v0[3:] + l1 * v1[3:] + l2 * v2[3:]
+                        fragments.append(Fragment(x, y, z, interp_data, s))
+        return fragments
+
+
+    def fragmentShader(self,fragment,data):
         N = fragment.interpolated_data[0:3]
+        N = N/np.linalg.norm(N)
         V = fragment.interpolated_data[3:6]
+        V = V/np.linalg.norm(V)
         L = fragment.interpolated_data[6:9]
-        uv = fragment.interpolated_data[9:11]
+        L = L/np.linalg.norm(L)
 
-        N_norm = np.linalg.norm(N)
-        V_norm = np.linalg.norm(V)
-        L_norm = np.linalg.norm(L)
+        R = 2 * np.dot(L,N) * N  -L
 
-        if N_norm > 0: N = N / N_norm
-        if V_norm > 0: V = V / V_norm
-        if L_norm > 0: L = L / L_norm
+        
 
-        R = 2 * np.dot(N, L) * N - L
-        R_norm = np.linalg.norm(R)
-        if R_norm > 0: R = R / R_norm
-
-        ambient = 0.2
-        diffuse = max(np.dot(N, L), 0.0)
-        specular = np.power(max(np.dot(R, V), 0.0), 16)
+        ambient = 1.0
+        diffuse = max(np.dot(N,L),0)
+        specular = np.power(max(np.dot(R,V),0.0),64) 
 
         ka = 0.1
-        kd = 0.8
-        ks = 0.5
+        kd = 0.9
+        ks = 0.3
+        phong = ka * ambient + kd * diffuse + ks * specular
+        phong = np.ceil(phong*4 +1 )/6.0
 
-        light_intensity = ka * ambient + kd * diffuse + ks * specular
-        tex = sample(data['texture'], uv[0], uv[1])
+        texture = sample(data['texture'], fragment.interpolated_data[9], fragment.interpolated_data[10])
 
-        fragment.output = np.clip(tex * light_intensity, 0, 1)
+
+        color = np.array([phong,phong,phong]) * texture
+
+        fragment.output = color
 
     def draw(self, vertices, triangles, data, MSAA=False, MSAA8x=False):
         self.image = np.zeros((self.height, self.width, 3))
-        self.msaaBuffer = np.zeros((self.height, self.width, self.samples, 3))
-        self.msaaDepth = np.ones((self.height, self.width, self.samples))
-        self.coverage = np.zeros((self.height, self.width), dtype=int)
+        
+        # Préparer les buffers selon le mode
+        if MSAA:
+            # Mode MSAA 
+            self.msaaBuffer = np.zeros((self.height, self.width, self.samples, 3))
+            self.msaaDepth = np.ones((self.height, self.width, self.samples))
+            self.coverage = np.zeros((self.height, self.width), dtype=int)
+        else:
+            # Mode normal - buffer simple
+            self.singleBuffer = np.zeros((self.height, self.width, 3))
+            self.singleDepth = np.ones((self.height, self.width))
+        if MSAA8x:
+            # Mode MSAA 8x
+            self.msaaBuffer = np.zeros((self.height, self.width, 8, 3))
+            self.msaaDepth = np.ones((self.height, self.width, 8))
+            self.coverage = np.zeros((self.height, self.width), dtype=int)
+        
         self.newVertices = np.zeros((vertices.shape[0], 14))
         for i in range(vertices.shape[0]):
             self.newVertices[i] = self.VertexShader(vertices[i], data)
+        
         fragments = []
         for i in triangles:
-            v0 = self.newVertices[i[0]]
-            v1 = self.newVertices[i[1]]
-            v2 = self.newVertices[i[2]]
             if MSAA:
-                fragments.extend(self.RasterizerMSAA(v0, v1, v2))
+                fragments.extend(self.RasterizerMSAA(self.newVertices[i[0]], self.newVertices[i[1]], self.newVertices[i[2]]))
+            elif MSAA8x:
+                fragments.extend(self.RasterizerMSAAx8(self.newVertices[i[0]], self.newVertices[i[1]], self.newVertices[i[2]]))
             else:
-                fragments.extend(self.Rasterizer(v0, v1, v2))
+                fragments.extend(self.Rasterizer(self.newVertices[i[0]], self.newVertices[i[1]], self.newVertices[i[2]]))
+        
         for f in fragments:
             self.fragmentShader(f, data)
-            if f.sample_id is None:
-                f.sample_id = 0
-            if self.msaaDepth[f.y, f.x, f.sample_id] > f.depth:
-                self.msaaDepth[f.y, f.x, f.sample_id] = f.depth
-                self.msaaBuffer[f.y, f.x, f.sample_id] = f.output
-                self.coverage[f.y, f.x] += 1
-        for y in range(self.height):
-            for x in range(self.width):
-                count = self.coverage[y, x]
-                if count > 0:
-                    self.image[y, x] = np.sum(self.msaaBuffer[y, x], axis=0) / count
+            
+            if MSAA or MSAA8x:
+                # Mode MSAA
+                if f.sample_id is None:
+                    f.sample_id = 0
+                if self.msaaDepth[f.y, f.x, f.sample_id] > f.depth:
+                    self.msaaDepth[f.y, f.x, f.sample_id] = f.depth
+                    self.msaaBuffer[f.y, f.x, f.sample_id] = f.output
+                    self.coverage[f.y, f.x] += 1  
+            else:
+                if self.singleDepth[f.y, f.x] > f.depth:
+                    self.singleDepth[f.y, f.x] = f.depth
+                    self.singleBuffer[f.y, f.x] = f.output
+        if MSAA or MSAA8x:
+            # Résolution MSAA
+            for y in range(self.height):
+                for x in range(self.width):
+                    if self.coverage[y, x] > 0:
+                        self.image[y, x] = np.sum(self.msaaBuffer[y, x], axis=0) / self.samples
+        else:
+            self.image = self.singleBuffer.copy()
